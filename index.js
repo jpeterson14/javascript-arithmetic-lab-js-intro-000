3f0515c50ce8e8a327e55d1ee93446f8669e9069
Jake Peterson <jpeterson0514@gmail.com>
2021-04-10 16:16:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function subtract(a,b) {
   return a - b
 }
 
-function multiply  (a,b)  {
+function multiply (a,b) {
   return a * b
 }
 
-divide = () => {
+function divide (a,b) {
   return a / b
 }
